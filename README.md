f5db838755b9a02d7b8ba5211d8fe6f2a2a8f37a
MatanAziza <matanaziza@gmail.com>
2026-03-06 17:44:50 +0100

    v1

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # Clickable-Grep
+
+This small code allows you to grep easily (on Linux) from your terminal into a folder and display hyperlinks to the found files.
+
+## Installation
+
+You can either git clone this repository using
+
+```bash
+git clone <https_key>
+```
+
+or by downloading the latest release.
+
+## How to run
+
+To run, you have 2 run options:
+
+- 1
+
+```bash
+./find_files.py <args>
+```
+
+- 2
+
+```bash
+python3 find_files.py <args>
+```
+
+As for the arguments to provide:
+
+- 1: Provide only the argument you're looking for, and the grep will search in the current directory and subdirectories:
+
+### Command
+
+```bash
+python3 find_files.py arg_1
+```
+
+### Result
+
+```terminal
+<span style="color: green">File n°1: GardenGuardian/ex4/ft_raise_errors.py</span>
+<span style="color: green">File n°2: CodeCultivation/ex4/ft_garden_security.py</span>
+<span style="color: green">File n°3: GardenGuardian/ex0/ft_first_exception.py</span>
+<span style="color: green">File n°4: CodeCultivation/ex5/ft_plant_types.py</span>
+<span style="color: green">File n°5: GardenGuardian/ex2/ft_custom_errors.py</span>
+<span style="color: green">File n°6: GardenGuardian/ex3/ft_finally_block.py</span>
+<span style="color: green">File n°7: CodeCultivation/ex0/ft_garden_intro.py</span>
+<span style="color: green">File n°8: GardenGuardian/ex5/ft_garden_management.py</span>
+```
+
---
+++ find_files.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+if __name__=="__main__":
+    args:list = []
+    for arg in sys.argv:
+        args.append("".join([c for c in arg if c != '\\']))
+    if len(args) == 1:
+        print("Missing searched argument.\nPlease run the program with what you're looking for or with argument '--help' to learn how to use it.")
+        sys.exit(1)
+    elif len(args) > 3:
+        print("Too much arguments were provided.\nPlease run the program with what you're looking for or with argument '--help' to learn how to use it.")
+        sys.exit(1)
+    one_arg = len(args) == 2
+    if args[1] == '--help':
+        print('help')
+        sys.exit(0)
+    where_to_search = input("What's the folder you want to look into?\n") if one_arg else args[2] # Where you want to search
+    folder: str = ""
+    tab = where_to_search.split('/')
+    folder = tab[-2] if not tab[-1] else tab[-1]
+    where_to_search = f'/home/{os.getlogin()}/{where_to_search[2:]}' if one_arg else where_to_search # Final path where to search
+    os.system("touch files.txt")
+    os.system(f"grep -rnw --include=*.py --include=*.c '{where_to_search}/' -e '.*{args[1]}.*'| sed 's/$/ 1/' >> files.txt")
+    file = open("files.txt", "r")
+    list_files = file.read().split("\n") # All occurences of searched string
+    file.close()
+    os.system("rm files.txt")
+    gen = list(set([file_path[:file_path.index(":")] for file_path in list_files[:-1]])) # List of unique path from /home
+    url_list = [f"file://{path}" for path in gen]
+    gen = [file_path[file_path.index(folder)+len(folder):] for file_path in gen] # Path from requested folder
+    if not gen:
+        print("\033[0;31mL'élément cherché n'existe pas dans le dossier et sous-dossiers!\033[1;37m")
+    for i, file_path in enumerate(gen):
+        path = file_path[file_path.index("/")+1:]
+        print(f"\033[0;32mFile n°{i+1}: \033[0m", end='')
+        print(f"\033]8;;{url_list[i]}\033\\{path}\033]8;;\033\\")
